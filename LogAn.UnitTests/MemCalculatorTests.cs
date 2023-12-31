2a91730e700db5e8417e33e699a378982b39ce61
Brian Futrell <wfutrell70@twc.com>
2023-12-31 11:33:02 -0500

    finished chapter 2

---
+++ LogAn.UnitTests/MemCalculatorTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace LogAn.UnitTests
+{
+    public class MemCalculatorTests
+    {
+        [Test]
+        public void Sum_ByDefault_ReturnsZero()
+        {
+            MemCalculator calculator = MakeCalc();
+
+            int lastSum = calculator.Sum();
+
+            Assert.That(lastSum, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void Add_WhenCalled_ChangesSum()
+        {
+            MemCalculator calculator = MakeCalc();
+
+            calculator.Add(1);
+            int sum = calculator.Sum();
+
+            Assert.That(sum, Is.EqualTo(1));
+        }
+
+        private static MemCalculator MakeCalc()
+        {
+            return new MemCalculator();
+        }
+    }
+}
